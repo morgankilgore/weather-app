7fce4ebf576e97531a7f5d77cde71e908fb96b84
Morgan Kilgore <mkilgore301@gmail.com>
2018-11-13 22:39:14 -0600

    Simplify returns from functions, catch connection error exception

--- weather_app/weather/utils/request.py
+++ weather_app/weather/utils/request.py
@@ -1,24 +1,25 @@
 import requests
 
 
-def get_request(url, params=None, to_dict=False):
+def get_request(url, params=None):
     """
     Sends a GET request to specified URL
 
     Param(s):
         url (str): Required. URL to send GET request to.
         params (dict): Optional. Parameters to be sent in the query string for the request.
-        to_dict (bool): Optional. True to return response as a dictionary, else False. Default False.
 
-    Returns: dict or request object. If 'to_dict' is True a dictionary is returned, else a Request object is returned.
+    Returns: tuple (bool, dict or str). (True, dict) if successful else (False, str).
     """
-    resp = requests.get(url=url, params=params)
-    if to_dict:
-        return resp.json()
-    return resp
+    try:
+        resp = requests.get(url=url, params=params)
+    except requests.exceptions.ConnectionError:
+        return False, 'Error in connecting to {}'.format(url)
+    else:
+        return True, resp.json()
 
 
-def post_request(url, params=None, json_data=None, to_dict=False):
+def post_request(url, params=None, json_data=None):
     """
     Sends a POST request to specified URL
 
@@ -26,11 +27,12 @@ def post_request(url, params=None, json_data=None, to_dict=False):
         url (str): Required. URL to send POST request to.
         params (dict): Optional. Parameters to be send in the query string for the request.
         json_data (dict): Optional. JSON data to send in the body of the request.
-        to_dict (bool). True to return response as a dictionary, else False. Default False.
 
-    Returns: dict or request object. If 'to_dict' is True a dictionary is returned, else a Request obejct is returned.
+    Returns: tuple (bool, dict or str). (True, dict) if successful else (False, str).
     """
-    resp = requests.post(url=url, params=params, json=json_data)
-    if to_dict:
-        return resp.json()
-    return resp
+    try:
+        resp = requests.post(url=url, params=params, json=json_data)
+    except requests.exceptions.ConnectionError:
+        return False, 'Error in connecting to {}'.format(url)
+    else:
+        return True, resp.json()
